6ad4f3ac3409c2e66359d006f93f490494f3fb6c
gyuha <nicegyuha@gmail.com>
2026-04-20 21:07:53 +0900

    feat: add Pino logger configuration

---
+++ src/bootstrap/logging/pino.config.ts
@@ -0,0 +1,18 @@
+import type { Params } from 'nestjs-pino';
+import { randomUUID } from 'crypto';
+
+export const pinoConfig: Params = {
+  pinoHttp: {
+    genReqId: (req) =>
+      (req.headers['x-trace-id'] as string | undefined) ?? randomUUID(),
+    level: process.env['NODE_ENV'] === 'production' ? 'info' : 'debug',
+    transport:
+      process.env['NODE_ENV'] !== 'production'
+        ? {
+            target: 'pino-pretty',
+            options: { colorize: true, singleLine: false },
+          }
+        : undefined,
+    autoLogging: true,
+  },
+};
